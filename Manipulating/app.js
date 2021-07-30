e27848788fecdd5b6acb4234b0690773e5b917b2
Blaise Gerstung <info@clearfeather.com>
2021-07-30 13:32:55 -0500

    updates

--- Manipulating/app.js
+++ Manipulating/app.js
@@ -1,5 +1,9 @@
 const allLinks = document.querySelectorAll('a');
 
+document.querySelector('h1').innerHTML = '<b>Greatest Title of All!!!!</b>'
+
+
+
 // for (let link of allLinks) {
 //     link.innerText = 'I AM A LINK!!!!'
 // }
@@ -9,4 +13,17 @@ for (let link of allLinks) {
     link.style.color = 'rgb(0, 108, 134)';
     link.style.textDecorationColor = 'magenta';
     link.style.textDecorationStyle = 'wavy'
+}
+
+///////////////////////////////////////////////////////////////
+
+// older methods for selection
+// getElementById
+// getElementsByTagName
+// getElementsByClassName
+
+const allImages = document.querySelectorAll('img');
+
+for (let img of allImages) {
+    img.src = 'https://source.unsplash.com/random'
 }
