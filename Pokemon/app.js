41b3edbb93ca86e7be309b159331e8e14e210ce5
Blaise Gerstung <info@clearfeather.com>
2021-08-07 11:08:54 -0500

    final

--- Pokemon/app.js
+++ Pokemon/app.js
@@ -1,20 +1,33 @@
-// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+// Select container for content
+const container = document.querySelector('#container');
 
-// const container = document.querySelector('#container');
-// const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+// Set base URL for images available
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
+// Loop through all available images
+for (i = 1; i <= 898; i++) {
 
-// for (let i = 1; i <= 151; i++) {
-//     const pokemon = document.createElement('div');
-//     pokemon.classList.add('pokemon');
-//     const label = document.createElement('span');
-//     label.innerText = `#${i}`;
-//     const newImg = document.createElement('img');
-//     newImg.src = `${baseURL}${i}.png`
+    // Add number to each pokemon :::
 
+    // Create div element
+    const pokemon = document.createElement('div');
+    // Add the class .pokemon to the pokemon div
+    pokemon.classList.add('pokemon');
+    // Create span element
+    const label = document.createElement('span');
+    // Add the number
+    label.innerText = `#${i}`;
 
-//     pokemon.appendChild(newImg);
-//     pokemon.appendChild(label);
-//     container.appendChild(pokemon)
-// }
+    // Display every pokemon :::
 
+    // Create img element
+    const newImg = document.createElement('img');
+    // Set image source to the base URL above, with i as the looped (increasing number), and png added to display the png image
+    newImg.src = `${baseURL}${i}.png`;
+    // Insert an img element
+    pokemon.appendChild(newImg);
+    // Insert the label span
+    pokemon.appendChild(label);
+    // Insert the completed pokemon div ino the container
+    container.appendChild(pokemon);
+}
