1b31172c123394eb85a2752f669d723ff49e6d37
Blaise Gerstung <info@clearfeather.com>
2021-08-10 16:33:43 -0500

    dom events practice

--- Selecting/app.js
+++ Selecting/app.js
@@ -1,18 +1,36 @@
-const allImages = document.getElementsByTagName('img');
+// // Selecting using JS
 
-for (let img of allImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+// OnClick to in-line function
+document.querySelector('h1').onclick = function () { alert('you clicked the H1'); }
+
+
+// Assign btn to id v2
+const btn = document.querySelector('#v2');
+
+// Separate function 
+ function clicked() {
+    console.log("You clicked me using app.js");
+}
+
+// Run function on click
+btn.onclick = clicked;
+
+
+// More events (hoisted)
+btn.onmouseenter = scream;
+btn.onmouseleave = bye;
+
+function scream() {
+    console.log('ahhhhhhh');
+}
+function bye() {
+    console.log('ok bye');
 }
 
 
-const squareImages = document.getElementsByClassName('square');
+// addEventListener sample - good for multiple callbacks, more customizable
 
-for (let img of squareImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
-}
-
-const links = document.querySelectorAll('p a');
-
-for (let link of links) {
-    console.log(link.href)
-}
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('click', function() {
+    alert('Clicked V3 button')
+})
