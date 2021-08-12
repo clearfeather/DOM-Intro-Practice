2fdc042748b367c79e94412d9bda24ea1e662c58
Blaise Gerstung <info@clearfeather.com>
2021-08-12 12:42:06 -0500

    update comments & code

--- FormEvents/app.js
+++ FormEvents/app.js
@@ -1,25 +1,42 @@
-
-
+// Select the form, assign to var
 const tweetForm = document.querySelector('#tweetForm');
-const tweetsContainer = document.querySelector('#tweets');
-tweetForm.addEventListener('submit', function (e) {
+// Select the ul for the tweets to be added
+const tweetContainer = document.querySelector('#tweets')
+
+// Start the event listener with the e parameter for the preventDefault and wait for submit event
+tweetForm.addEventListener('submit', function(e) {
+
+    // do not go to a new page if not needed, stay on same page
     e.preventDefault();
 
-    // const usernameInput = document.querySelectorAll('input')[0];
-    // const tweetInput = document.querySelectorAll('input')[1];
+    // Select each form input by way of using elements
     const usernameInput = tweetForm.elements.username;
     const tweetInput = tweetForm.elements.tweet;
-    addTweet(usernameInput.value, tweetInput.value)
+
+    //Pass inputs into addTween function
+    addTweet(usernameInput.value, tweetInput.value);
+
+    //Set both values to empty strings once submitted
     usernameInput.value = '';
     tweetInput.value = '';
-});
+    
+})
 
+// Separate function to add a tweet
 const addTweet = (username, tweet) => {
+
+    // Create li and bold tags
     const newTweet = document.createElement('li');
     const bTag = document.createElement('b');
-    bTag.append(username)
-    newTweet.append(bTag);
-    newTweet.append(`- ${tweet}`)
-    tweetsContainer.append(newTweet);
-}
 
+    // Add username text inside of the bold tag
+    bTag.append(username);
+    // Add bold username inside li
+    newTweet.append(bTag);
+    // Add the tweet text to the completed li
+    newTweet.append(` - ${tweet}`);
+    
+    // Add the completed tweet to the container
+    tweetContainer.append(newTweet);
+    
+}
